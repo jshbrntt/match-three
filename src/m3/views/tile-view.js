e2d063be97965dc2388e83f5b4439e1fed19d5ec
Joshua Barnett <jbacontrol@gmail.com>
2016-03-13 20:10:14 +0000

    Started rendering TileView as a double sided Plane instead of a Sprite.

--- src/m3/views/tile-view.js
+++ src/m3/views/tile-view.js
@@ -10,24 +10,39 @@ export default class TileView extends View {
     if (model.value > textures.length - 1) {
       throw new Error("No texture exists for this tile's value");
     }
-    this._sprite = new THREE.Sprite(textures[model.value]);
-    this._tweenQueue = [];
-    this._callbackQueue = [];
 
-    this._model._onMoved = this.onMoved;
+    this._texture  = textures[this._model.value];
+    this._geometry = new THREE.PlaneGeometry(this._texture.image.width, this._texture.image.height);
+    this._material = new THREE.MeshBasicMaterial({
+      map: this._texture,
+      side: THREE.DoubleSide,
+      transparent: true,
+      depthWrite: false,
+      depthTest: false
+    });
+    this._plane            = new THREE.Mesh(this._geometry, this._material);
+    this._plane.position.x = this._geometry.parameters.width / 2;
+    this._plane.position.y = this._geometry.parameters.height / 2;
+
+    this._tweenQueue       = [];
+    this._callbackQueue    = [];
+
+    this._model._onMoved   = this.onMoved;
     this._model._onRemoved = this.onRemoved;
 
-    this._sprite.scale.set(
-      this._sprite.material.map.image.width,
-      this._sprite.material.map.image.height,
-      1
-    );
+    this.add(this._plane);
 
-    this._sprite.position.set(
-      this._model.cell.x * this._sprite.scale.x,
-      this._model.cell.y * this._sprite.scale.y,
-      1
-    );
+    // this._sprite.scale.set(
+    //   this._sprite.material.map.image.width,
+    //   this._sprite.material.map.image.height,
+    //   1
+    // );
+    //
+    // this._sprite.position.set(
+    //   this._model.cell.x * this._sprite.scale.x,
+    //   this._model.cell.y * this._sprite.scale.y,
+    //   1
+    // );
 
   }
   onMoved(cell, time, onFinished) {
@@ -46,6 +61,11 @@ export default class TileView extends View {
     // this._tweenQueue.push(tween);
     // this._callbackQueue.push(onFinished);
   }
+  update() {
+    // this._plane.rotation.x += Math.random() * .05;
+    // this._plane.rotation.y += .05;
+    // this._plane.rotation.z += .05;
+  }
   onRemoved() {
     this._sprite.parent.remove(this._sprite);
   }
@@ -60,6 +80,12 @@ export default class TileView extends View {
   onUpdated() {
     super.onUpdated();
     // TODO: Highlight here relative to this._model.highlight
+  }
+  get wireframe() {
+    return this._material.wireframe;
+  }
+  set wireframe(value) {
+    return this._material.wireframe = value;
   }
   get moving() {
     return this._tweenQueue.length > 0;
