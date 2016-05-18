0a983befa82f1cd4e89212cd04b73d1dfe1ccc45
Joshua Barnett <jbacontrol@gmail.com>
2016-05-18 22:11:32 +0100

    Autoformatting.

--- src/core/mouse-event.js
+++ src/core/mouse-event.js
@@ -1,7 +1,7 @@
 let MouseEvent = {
-  DOWN: 'core.MouseEvent.DOWN',
-  UP: 'core.MouseEvent.UP',
+  DOWN:  'core.MouseEvent.DOWN',
+  UP:    'core.MouseEvent.UP',
   CLICK: 'core.MouseEvent.CLICK',
-  MOVE: 'core.MouseEvent.MOVE'
+  MOVE:  'core.MouseEvent.MOVE'
 };
 export default MouseEvent;
