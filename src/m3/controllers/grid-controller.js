b45b7fc8f1fa4deb34d9cb04493093450205d873
Joshua Barnett <jbacontrol@gmail.com>
2016-05-18 22:14:27 +0100

    Updating grid controller to use mouse or touch services relative to feature availability.

--- src/m3/controllers/grid-controller.js
+++ src/m3/controllers/grid-controller.js
@@ -1,6 +1,8 @@
 import THREE from 'three';
+import feature from 'feature';
 import Controller from './../../core/mvc/controller';
 import ServiceLocator from './../../core/service-locator';
+import TouchEvent from './../../core/touch-event';
 import MouseEvent from './../../core/mouse-event';
 import TileView from './../views/tile-view';
 
@@ -8,28 +10,48 @@ export default class GridController extends Controller {
   constructor(gridModel, gridView) {
     super(gridModel, gridView);
     this._camera = ServiceLocator.get('M3Game').camera;
-    this._mouse = ServiceLocator.get('Mouse');
-    this._mouse.addEventListener(MouseEvent.DOWN, this.onMouseDown.bind(this));
+
+    if (feature.touch) {
+      this._input = ServiceLocator.get('Touch');
+      this._input.addEventListener(TouchEvent.START, this.onMouseDown.bind(this));
+      this._input.addEventListener(TouchEvent.END,   this.onMouseUp.bind(this));
+    }
+    else {
+      this._input = ServiceLocator.get('Mouse');
+      this._input.addEventListener(MouseEvent.DOWN, this.onMouseDown.bind(this));
+      this._input.addEventListener(MouseEvent.UP,   this.onMouseUp.bind(this));
+    }
+
     this._raycaster = new THREE.Raycaster();
     this._selected = [];
   }
-  getLocation() {
-    this._raycaster.setFromCamera(this._mouse.position, this._camera);
+  getIntersects() {
+    this._raycaster.setFromCamera(this._input.position, this._camera);
     let intersects = this._raycaster.intersectObjects(this.view.children, true);
-    if (intersects.length > 0) {
-      let view = intersects[0].object.parent;
-      if (view instanceof TileView) {
-        console.log(view.model.cell);
-        this._selected.push(view);
-      }
-    }
-    if (this._selected.length === 2) {
-      this.model.swapCells(this._selected[0].model.cell, this._selected[1].model.cell);
-      this._selected = [];
-      console.log(this.model.toString());
+    return intersects;
+  }
+  selectTileView(view) {
+    if (view instanceof TileView) {
+      this._selected.push(view);
+      view.highlight = true;
     }
   }
+  swapSelectedTiles() {
+      this.model.swapCells(this._selected[0].model.cell, this._selected[1].model.cell);
+      for (let selected of this._selected) {
+        selected.highlight = false;
+      }
+      this._selected = [];
+  }
   onMouseDown(event) {
-    this.getLocation();
+    console.debug('onMouseDown', event);
+    let intersects = this.getIntersects();
+    this.selectTileView(intersects[0].object.parent);
+  }
+  onMouseUp(event) {
+    console.debug('onMouseUp', event);
+    if (this._selected.length === 2) {
+      this.swapSelectedTiles();
+    }
   }
 }
