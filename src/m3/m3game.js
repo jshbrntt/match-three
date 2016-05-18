bcb64bfd3d70c779636ff2ceb04e7bd996e7bc26
Joshua Barnett <jbacontrol@gmail.com>
2016-05-18 22:13:23 +0100

    Proving instance of touch service to the game class.

--- src/m3/m3game.js
+++ src/m3/m3game.js
@@ -1,5 +1,6 @@
 import Game from './../core/game';
 import Mouse from './../core/mouse';
+import Touch from './../core/touch';
 import M3Scene from './m3scene';
 import TWEEN from 'tween';
 import ServiceLocator from './../core/service-locator';
@@ -8,6 +9,7 @@ export default class M3Game extends Game {
   constructor(renderer) {
     super(renderer);
     ServiceLocator.provide(new Mouse());
+    ServiceLocator.provide(new Touch());
   }
   start() {
     super.start();
