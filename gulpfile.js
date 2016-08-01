c25792cf6de095f94a94599e8f71a77437c2070b
Joshua Barnett <jbacontrol@gmail.com>
2016-08-01 18:31:04 +0100

    Created a production and dev build task.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,7 @@ let WebpackDevServer = require('webpack-dev-server');
 let webpack          = require('webpack');
 let ghPages          = require('gulp-gh-pages');
 
-gulp.task('build', (callback) => {
-  let config = require('./webpack.config.js');
+function build(config, callback) {
   webpack(config, (err, stats) => {
     if (err) throw new gutil.PluginError("webpack", err);
     stats.toString(config.devServer.stats).split('\n').map((line) => {
@@ -14,15 +13,40 @@ gulp.task('build', (callback) => {
     });
     callback();
   });
+}
+
+gulp.task('build:dev', (callback) => {
+  let config = require('./webpack.config');
+  return build(config, callback);
 });
 
-gulp.task('deploy', ['build'], () => {
+gulp.task('build:production', (callback) => {
+  let config = require('./webpack.config');
+  config.plugins = [...config.plugins,
+    new CleanWebpackPlugin([config.output.path]),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+  ];
+  config.output = JSON.parse(JSON.stringify(config.output).replace(/.js/g, `.${Date.now()}.min.js`));
+  return build(config, callback);
+});
+
+gulp.task('deploy', ['build:production'], () => {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
 
-gulp.task('default', (callback) => {
-  let config = require('./webpack.config.js');
+gulp.task('watch', (callback) => {
+  let config = require('./webpack.config');
   config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
   new WebpackDevServer(new webpack(config), config.devServer)
     .listen(8080, 'localhost', (err) => {
