bc5673b958d4ac881bb4fd3efd8ce3cf6ac2ed00
Joshua Barnett <jbacontrol@gmail.com>
2016-10-30 15:59:42 +0000

    Aligning import statements in webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-'use strict';
-const webpack = require('webpack');
-const path = require('path');
+const webpack            = require('webpack');
+const path               = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
+const autoprefixer       = require('autoprefixer');
 
 let config = {};
 
