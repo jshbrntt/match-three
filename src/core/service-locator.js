772805efd229c675f833d9b4cccb454dd6cb2cb3
Joshua Barnett <jbacontrol@gmail.com>
2017-06-06 20:13:28 +0100

    Check the service locator map is initialized before attempting a look up.

--- src/core/service-locator.js
+++ src/core/service-locator.js
@@ -6,9 +6,9 @@ export default class ServiceLocator {
     this._services.set(name, service)
   }
   static get (name) {
-    if (this._services.has(name)) {
+    if (this._services && this._services.has(name)) {
       return this._services.get(name)
-    }else {
+    } else {
       throw new ReferenceError(`Service '${name}' not found.`)
     }
   }
