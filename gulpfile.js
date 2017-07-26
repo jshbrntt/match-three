0ffcddd9fd37dbb499d9022a6c3622468a5316e9
Joshua Barnett <jbacontrol@gmail.com>
2017-07-26 22:30:23 +0200

    Refactor gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,22 @@
-const cleanWebpackPlugin = require('clean-webpack-plugin');
-const config             = require('config');
-const express            = require('express');
-const ghPages            = require('gulp-gh-pages');
-const gulp               = require('gulp');
-const gutil              = require('gulp-util');
-const http               = require('http');
-const MultiplayerServer  = require('./server/server.js');
-const path               = require('path');
-const webpack            = require('webpack');
-const webpackDevServer   = require('webpack-dev-server');
-const webpackConfig      = require('./webpack.config.js');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ghPages = require('gulp-gh-pages')
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const Webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const { IcosaServer } = require('./icosa')
 
-function build(config, callback) {
-  webpackConfig.plugins = [...webpackConfig.plugins,
-    new cleanWebpackPlugin([webpackConfig.output.path])
-  ];
-  webpack(webpackConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    stats.toString(webpackConfig.devServer.stats).split('\n').map((line) => {
-      gutil.log(gutil.colors.blue("[webpack]"), line);
-    });
-    callback();
-  });
+function build (config, callback) {
+  config.plugins = [...config.plugins,
+    new CleanWebpackPlugin([config.output.path])
+  ]
+  Webpack(config, (err, stats) => {
+    if (err) throw new gutil.PluginError('webpack', err)
+    stats.toString(config.devServer.stats).split('\n').map((line) => {
+      gutil.log(gutil.colors.blue('[webpack]'), line)
+    })
+    callback()
+  })
 }
 
 gulp.task('multiplayer', () => {
@@ -31,50 +26,41 @@ gulp.task('multiplayer', () => {
 });
 
 gulp.task('build:dev', (callback) => {
-  let webpackConfig = require('./webpack.config');
-  webpackConfig.devtool = 'source-map';
-  return build(webpackConfig, callback);
-});
+  let config = require('./webpack.config')
+  config.devtool = 'source-map'
+  return build(config, callback)
+})
 
 gulp.task('build', (callback) => {
-  let webpackConfig = require('./webpack.config');
-  webpackConfig.plugins = [...webpackConfig.plugins,
-    new webpack.DefinePlugin({
-      "process.env": {
-        "NODE_ENV": JSON.stringify("production")
+  let config = require('./webpack.config')
+  config.plugins = [...config.plugins,
+    new Webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
+    new Webpack.optimize.DedupePlugin(),
+    new Webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
       }
     })
-  ];
-  webpackConfig.output = JSON.parse(JSON.stringify(webpackConfig.output).replace(/.js/g, `.${Date.now()}.min.js`));
-  return build(webpackConfig, callback);
-});
+  ]
+  config.output = JSON.parse(JSON.stringify(config.output).replace(/.js/g, `.${Date.now()}.min.js`))
+  return build(config, callback)
+})
 
-gulp.task('deploy', ['build:production'], () => {
+gulp.task('deploy', ['build'], () => {
   return gulp.src('./dist/**/*')
-    .pipe(ghPages());
-});
+    .pipe(ghPages())
+})
 
-gulp.task('watch', ['multiplayer'], (callback) => {
-  let webpackConfig = require('./webpack.config');
-  webpackConfig.devtool = 'source-map';
-  webpackConfig.entry.app.unshift(`webpack-dev-server/client?http://${config.get('app.host')}:${config.get('app.port')}/`);
-  new webpackDevServer(new webpack(webpackConfig), webpackConfig.devServer)
-    .listen(config.get('app.port'), config.get('app.host'), (err) => {
-      if (err) throw new gutil.PluginError('webpack-dev-server', err);
-      gutil.log(gutil.colors.cyan(`http://${config.get('app.host')}:${config.get('app.port')}`));
-    });
-});
-
-gulp.task('app', ['multiplayer'], () => {
-  let appServer = express();
-  appServer.listen(config.get('app.port'), config.get('app.host'));
-  appServer.use(express.static(path.join(__dirname, 'dist')));
-});
-
-gulp.task('default', ['app', 'multiplayer']);
+gulp.task('dev', done => {
+  let server = new IcosaServer(3000)
+  let config = require('./webpack.config')
+  new WebpackDevServer(new Webpack(config), config.devServer)
+    .listen(config.devServer.port, config.devServer.host, err => {
+      if (err) throw new gutil.PluginError('webpack-dev-server', err)
+      gutil.log(gutil.colors.cyan(`${config.devServer.https ? 'https' : 'http'}://${config.devServer.host}:${config.devServer.port}`))
+    })
+})
