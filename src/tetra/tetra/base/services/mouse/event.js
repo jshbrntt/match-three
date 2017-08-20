bf1fee52ccc9015e58650a2dbb61c194b492af5d
Joshua Barnett <jbacontrol@gmail.com>
2017-08-20 19:21:03 +0100

    Add wheel mouse event to constant.

--- src/tetra/tetra/base/services/mouse/event.js
+++ src/tetra/tetra/base/services/mouse/event.js
@@ -1,7 +1,8 @@
 let MouseEvent = {
-  DOWN: 'core.MouseEvent.DOWN',
-  UP: 'core.MouseEvent.UP',
-  CLICK: 'core.MouseEvent.CLICK',
-  MOVE: 'core.MouseEvent.MOVE'
+  DOWN: 'tetra.base.services.mouse.MouseEvent.DOWN',
+  UP: 'tetra.base.services.mouse.MouseEvent.UP',
+  CLICK: 'tetra.base.services.mouse.MouseEvent.CLICK',
+  MOVE: 'tetra.base.services.mouse.MouseEvent.MOVE',
+  WHEEL: 'tetra.base.services.mouse.MouseEvent.WHEEL'
 }
 export default MouseEvent
