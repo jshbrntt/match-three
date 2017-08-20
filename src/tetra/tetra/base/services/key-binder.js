94c5987dc6879835d850dee9d2b6a32a0db34436
Joshua Barnett <jbacontrol@gmail.com>
2017-08-20 19:19:31 +0100

    Update KeyBinder service to record state of keys, so checking for held keys is easier.

--- src/tetra/tetra/base/services/key-binder.js
+++ src/tetra/tetra/base/services/key-binder.js
@@ -3,22 +3,54 @@ import document from 'document'
 export default class KeyBinder {
   constructor (bindings) {
     this.bindings = bindings
+    this.states = {}
+    this.enabled = false
     this.enable()
   }
+  held (code) {
+    if (code in this.states) {
+      return this.states[code]
+    }
+    return false
+  }
   enable () {
-    document.addEventListener('keydown', this.handleKeyDown.bind(this))
+    if (!this.enabled) {
+      document.addEventListener('keydown', this.handleKeyDown.bind(this))
+      document.addEventListener('keyup', this.handleKeyUp.bind(this))
+    }
   }
   disable () {
-    document.removeEventListener('keydown', this.handleKeyDown.bind(this))
+    if (this.enabled) {
+      document.removeEventListener('keydown', this.handleKeyDown.bind(this))
+      document.removeEventListener('keyup', this.handleKeyUp.bind(this))
+    }
+  }
+  updateState (keyInfo, state) {
+    this.states[keyInfo.code] = state
+    this.states[keyInfo.key] = state
+  }
+  callBindings (keyInfo, code) {
+    if (keyInfo.string in this.bindings) {
+      this.bindings[keyInfo.string]()
+    }
+    if (keyInfo.code in this.bindings) {
+      this.bindings[keyInfo.code]()
+    }
+  }
+  getKeyInfo (event) {
+    return {
+      code: event.which,
+      string: String.fromCharCode(event.which)
+    }
   }
   handleKeyDown (event) {
-    let code = event.which
-    let key = String.fromCharCode(code)
-    if (key in this.bindings) {
-      this.bindings[key]()
-    }
-    if (code in this.bindings) {
-      this.bindings[code]()
-    }
+    let keyInfo = this.getKeyInfo(event)
+    this.updateState(keyInfo, true)
+    this.callBindings(keyInfo)
+  }
+  handleKeyUp (event) {
+    let keyInfo = this.getKeyInfo(event)
+    this.updateState(keyInfo, false)
+    this.callBindings(keyInfo)
   }
 }
